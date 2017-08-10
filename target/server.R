ed30da8874f91e96576e3f8cff537574323bb44a
mickey7799 <vickey.lee@taitra.org.tw>
2017-08-10 09:56:33 +0800

    Update Chinese version

--- target/server.R
+++ target/server.R
@@ -4,23 +4,25 @@ library(ggplot2)
 library(dplyr)
 library(readr)
 library(RColorBrewer)
-
+library(readxl)
+library(purrr)
 
 function(input, output) {
-  data <- read_csv("data/outputB.csv")
+  data <- read_excel("data/outputD.xlsx")
+  data[,c(14:16,20:21)] <- map(data[,c(14:16,20:21)],as.numeric)
   
-  group.list <- c('All', as.character(unique(data$label)) %>% sort())
+  group.list <- c('All', as.character(unique(data$`群組`)) %>% sort())
   output$groupSelector <- renderUI({selectInput('group', '群組:', as.list(group.list))})
-  area.list <- c('All', as.character(unique(data$region)) %>% sort())
+  area.list <- c('All', as.character(unique(data$`地區`)) %>% sort())
   output$areaSelector <- renderUI({selectInput('area', '地區:', as.list(area.list))})
   
   data_temp <- reactive({
     
     if (input$group != "All") {
-      data <- data[data$label == input$group,]
+      data <- data[data$`群組` == input$group,]
     }
     if (input$area != "All") {
-      data <- data[data$region == input$area,]
+      data <- data[data$`地區` == input$area,]
     }
     # if (input$hscode != "All") {
     #   data <- data[data$HSCODE == input$hscode,]
@@ -30,14 +32,18 @@ function(input, output) {
   
   conditions <- reactive({
     list(
-      as.vector(scale(data_temp()$export.pred)) >= -0.1907,
-      data_temp()$im_growth > 0,
-      data_temp()$nrca.ttl > 0,
-      data_temp()$herf < 0.1,
-      as.vector(scale(data_temp()$share.ttl)) >= 0.3614,
-      as.vector(scale(data_temp()$company_n)) >= -0.02845,
-      as.vector(scale(data_temp()$company_n8000)) >= -0.06453,
-      data_temp()$risk %in% c('AA','A'),
+      as.vector(scale(data_temp()$`出口額預測2018`)) >= -0.1708,
+      data_temp()$`進口成長預測2018` > 0,
+      data_temp()$`臺灣產業在該市場的相對優勢度` > 0,
+      data_temp()$`各國自臺進口的產品集中度` < 0.1,
+      as.vector(scale(data_temp()$`臺灣產業在該市場的進口市占變化`)) >= 0.3614,
+      as.vector(scale(data_temp()$`出口商數`)) >= -0.02845,
+      as.vector(scale(data_temp()$`中小企業出口商數`)) >= -0.06453,
+      data_temp()$`市場風險` %in% c('AA','A'),
+      data_temp()$`商機總量2016` > mean(data_temp()$`商機總量2016`),
+      data_temp()$`觀展公司數2016` > mean(data_temp()$`觀展公司數2016`),
+      data_temp()$`TT流量201609至201706` > mean(data_temp()$`TT流量201609至201706`),
+      data_temp()$`展覽補助總核准次數2016` > mean(data_temp()$`展覽補助總核准次數2016`),
       # default condition
       rep(T, nrow(data_temp()))
     )
@@ -46,9 +52,9 @@ function(input, output) {
   data_filtered <- reactive({
     
     # Filter table based on selected conditions
-    data <- data_temp()[Reduce(`&`, conditions()[as.integer(c(input$macro_criteria, 9))]), ]
+    data <- data_temp()[Reduce(`&`, conditions()[as.integer(c(input$macro_criteria, 13))]), ]
     # Remove empty rows
-    data[!is.na(data$reporter), ]
+    data[!is.na(data$`國家`), ]
     
   })
   
@@ -69,32 +75,21 @@ function(input, output) {
       write.csv(data_filtered(), file)
     }
   )
-#-----------------------------------------------------------------------  
+  #-----------------------------------------------------------------------  
   
   x.list <- as.character(names(data)[c(6,8:10,14:16)]) %>% sort()
-  output$xSelector <- renderUI({selectInput('x', 'X軸變數:', as.list(x.list), selected='grate.pred')})  
+  output$xSelector <- renderUI({selectInput('x', 'X軸變數:', as.list(x.list), selected='出口成長率預測2018')})  
   y.list <- as.character(names(data)[c(6,8:10,14:16)]) %>% sort()
-  output$ySelector <- renderUI({selectInput('y', 'Y軸變數:', as.list(y.list), selected='gdp_growth')})
-  size.list <- as.character(names(data)[c(7,11:13,17)])%>%sort()
-  output$sizeSelector <- renderUI({selectInput('size', '泡泡大小變數:', as.list(size.list), selected='export.pred')})  
+  output$ySelector <- renderUI({selectInput('y', 'Y軸變數:', as.list(y.list), selected='GDP成長預測2018')})
+  size.list <- as.character(names(data)[c(7,11:13,17,22:39)])%>%sort()
+  output$sizeSelector <- renderUI({selectInput('size', '泡泡大小變數:', as.list(size.list), selected='出口額預測2018')})  
   
-  #filter data for plotting
-  data_plot <- reactive({
-    
-    data <- data_filtered()[, c(input$x, input$y, input$size)]
-    #data_filtered() <- data.frame(lapply(data_filtered()[,c(4:8,11:13,15:16)], function(x) {(x - min(x))/(max(x) - min(x))}))
-    data
-  })
-  
-  #output$table2 <- DT::renderDataTable(DT::datatable({
-  #  data_plot()
-  #}))
   
   output$bubble <- renderPlot({
     clr <- colorRampPalette(brewer.pal(11, 'Spectral'))
-    ggplot(data_filtered(), aes_string(x = input$x, y = input$y, size = input$size , fill = 'label')) +
+    ggplot(data_filtered(), aes_string(x = input$x, y = input$y, size = input$size , fill = '群組')) +
       geom_point(alpha = 0.6, shape = 21, col = 'grey') +
-      geom_text(aes_string(x = input$x, y = input$y, label = 'reporter'), size = 3) +
+      geom_text(aes_string(x = input$x, y = input$y, label = '國家'), size = 3) +
       geom_vline(xintercept = 0) +
       geom_hline(yintercept = 0) +
       ggtitle( paste(as.character(input$x), 'vs', as.character(input$y))) +
@@ -103,8 +98,9 @@ function(input, output) {
       theme(plot.title = element_text(size = 24, face = 'bold'),
             axis.title = element_text(size = 16, face = 'bold'),
             axis.text = element_text(face = 'bold')) +
-      geom_vline(xintercept = 3.6, linetype = 2, col = 'red') +
-      geom_hline(yintercept = 5.08, linetype = 2, col = 'red')
+      geom_vline(xintercept = median(data_filtered()[[input$x]]), linetype = 2, col = 'red') +
+      geom_hline(yintercept = median(data_filtered()[[input$y]]), linetype = 2, col = 'red')
+    
     
   })
   
--- target/ui.R
+++ target/ui.R
@@ -23,24 +23,34 @@ fluidPage(
       checkboxGroupInput('macro_criteria', label = h5(strong('選擇市場篩選條件')),#, style = 'color:#12d5ff'),
                          choices = list('具出口規模' = 1, '整體進口正成長' = 2, '臺灣產業具相對優勢度' = 3, 
                                         '自臺進口產品集中度低' = 4, '臺灣進口市占增加大' = 5,
-                                        '高出口商家數' = 6, '多中小企業出口商' = 7, '市場風險低' = 8),  
+                                        '高出口商家數' = 6, '多中小企業出口商' = 7, '市場風險低' = 8,
+                                        '2016年商機總量大於平均' = 9, '2016年觀展公司數大於平均' = 10, 'TT流量大於平均' = 11,
+                                        '2016年展覽補助總核准次數大於平均' = 12),  
                          selected = NULL),
-
- #--------------------------------------------------------------------   
+      
+      #--------------------------------------------------------------------   
       br(),
       h4('篩選結果指標視覺化', align = 'left'),
       uiOutput('xSelector'),
       uiOutput('ySelector'), 
-      uiOutput('sizeSelector')
+      uiOutput('sizeSelector'),
+      br(),
+      p('群組描述：', align = 'left'),
+      p('A: 中低規模、中高成長、中高風險市場', align = 'left'),
+      p('B: 中上規模、中高成長、低風險市場', align = 'left'),
+      p('C: 小規模、中低成長、高風險市場', align = 'left'),
+      p('D: 大規模、中成長、低風險市場', align = 'left'),
+      p('E: 極端值或未分群市場', align = 'left')
+      
     ), 
- 
+    
     # Create a new row for the table.
     mainPanel(
       h4('【市場篩選結果】', align = 'center'),
       h4('共有', htmlOutput('wording', inline = T), '個市場符合篩選條件：', align = 'center'),
       DT::dataTableOutput("table"),
       downloadButton('downloadData', 'Download', align='right'),
-#------------------------------------------------------------------------      
+      #------------------------------------------------------------------------      
       #graph
       br(),
       br(),
